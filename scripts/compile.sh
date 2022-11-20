ab403005601db5d6274fdc89b7cf7d86f4414d95
Justin Lee <2025jlee@tjhsst.edu>
2022-11-20 12:37:56 -0500

    add size func to sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,16 +4,20 @@ set -e
 
 rm -f ./bin/*
 
+size() {
+	echo $(du -hs $1 | cut -f1)
+}
+
 for file in ./src/*.cpp; do
 	base=$(basename $file .cpp)
 	g++ -O2 -c $file -o "./bin/$base.o" $@
-	echo "Compiled \`$base.cpp\` ($(du -hs ./bin/$base.o | cut -f1))"
+	echo "Compiled \`$base.cpp\` ($(size ./bin/$base.o))"
 done
 
 for file in ./*.cpp; do 
 	base=$(basename $file .cpp)
 	g++ -O2 -s $file ./bin/*.o -o "./bin/$base.out" -l crypto -l jsoncpp $@ 
-	echo "Compiled \`$base.cpp\` ($(du -hs ./bin/$base.out | cut -f1))"
+	echo "Compiled \`$base.cpp\` ($(size ./bin/$base.out))"
 done
 
-echo "Total binary size: $(du -hs ./bin | cut -f1)"
+echo "Total binary size: $(size ./bin)"
